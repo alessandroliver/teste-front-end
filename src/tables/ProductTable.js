a86232cbefd19a44239c3852dcc6614b91950a81
alessandroliver <alessandrorodrgues270@gmail.com>
2020-03-20 14:42:03 -0300

    Add product table

---
+++ src/tables/ProductTable.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const ProductTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Código</th>
+        <th>Modelo</th>
+        <th>Preço</th>
+        <th>Marca</th>
+        <th>Cor</th>
+        <th>Início das vendas</th>
+        <th>Fim das vendas</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.products.length > 0 ? (
+        props.products.map(product => (
+          <tr key={product.id}>
+            <td>{product.id}</td>
+            <td>{product.model}</td>
+            <td>{product.price}</td>
+            <td>{product.brand}</td>
+            <td>{product.color}</td>
+            <td>{product.startDate}</td>
+            <td>{product.endDate}</td>
+            <td>
+              <button
+                onClick={() => {
+                  props.editRow(product)
+                }}
+                className="button muted-button"
+              >
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+              </button>
+              <button
+                onClick={() => props.deleteProduct(product.id)}
+                className="button muted-button"
+              >
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>Sem produtos</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
+
+export default ProductTable
